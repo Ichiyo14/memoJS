7f6604be9227a07618935aceab204c8a68e8f374
yoichi <itiyou14@gmail.com>
2021-08-21 17:47:59 +0900

    lコマンドの実装とstrage.jsonをgitignoreにした

--- memo.js
+++ memo.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const stragePath = './strage.json'
+const argv = require('minimist')(process.argv.slice(2))
 
 if (!fs.existsSync(stragePath)) {
   console.log('jsonファイルを作成する')
@@ -16,20 +17,22 @@ class Memo {
     process.stdin.setEncoding('utf8')
     const lines = []
     const reader = require('readline').createInterface({
-      input: process.stdin,
-      output: process.stdout
+      input: process.stdin
     })
     reader.on('line', (line) => {
       lines.push(line + '\n')
     })
     reader.on('close', () => {
       lines.splice(-1, 1, lines[lines.length - 1].trim())
-      console.log(this.memosDeta)
-      console.log(lines)
       this.memosDeta.memos.push(lines)
-      console.log(this.memosDeta)
       this.strage.write(this.memosDeta)
     })
+  }
+
+  list () {
+    for (const i in this.memosDeta.memos) {
+      console.log(this.memosDeta.memos[i][0].replace(/\r?\n/g, ''))
+    }
   }
 }
 
@@ -47,3 +50,4 @@ class Strage {
 const strage = new Strage(stragePath)
 const memo = new Memo(strage)
 if (!process.stdin.isTTY) { memo.add() }
+if (argv.l) memo.list()
